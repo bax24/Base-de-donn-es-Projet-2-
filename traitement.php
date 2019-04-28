b475b2c880bc60784a4fd7278d8338b584a406db
Lucas Backes <lucas.backes07@gmail.com>
2019-04-28 11:50:45 +0200

    init liste déroulante

--- traitement.php
+++ traitement.php
@@ -8,12 +8,40 @@
 <body>
 
 	<?php 
-	if (isset($_POST['passwd']) AND $_POST['passwd'] ==  "hey" AND isset($_POST['login']) AND $_POST['login'] ==  "groupe22") { 
+	if (isset($_POST['passwd']) AND $_POST['passwd'] ==  "p" AND isset($_POST['login']) AND $_POST['login'] ==  "groupe22") { 
 	?>
 
-		<p> je suis dedans </p>
+	<div class="login">
+		<form method="post">
+			<select name="request"> 
+				<option>Afficher des tables</option>
+				<option>question 2</option>
+				<option>question 3</option>
+			</select>
+			<p>
+				<input type="submit" value="SEARCH" name="search" class="input2">
+			</p>
+		</form>
 
-		
+
+
+	<form method="post" action="request/AfficheTables.php">
+
+			<select name="tables">
+				<option>Auteurs</option>
+				<option>Institutions</option>
+				<option>Articles</option>
+				<option>Sujets</option>
+				<option>Second auteur</option>
+				<option>Articles journal</option>
+				<option>Article conférence</option>
+				<option>Revue</option>
+				<option>Conférence</option>
+				<option>Participation conférence</option>
+			</select>
+		</div>
+	</form>
+
 
 	<?php 
 
@@ -22,6 +50,7 @@
 	else{
 	?>
 		<p> hey baby u're not in</p>
+
 	<?php  
 		}
 	?>
