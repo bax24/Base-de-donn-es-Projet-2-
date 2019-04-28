ac95cf674ef1c41fc91a599867281cda72d48cfb
Lucas Backes <lucas.backes07@gmail.com>
2019-04-28 11:51:31 +0200

    queries des questions de l'énoncé

---
+++ query.sql
@@ -0,0 +1,15 @@
+--Question B
+
+SELECT matricule_premier_auteur, titre, date_publication, 'articles de journal' AS type FROM articles, articles_journaux 
+WHERE articles.url = articles_journaux.url AND articles.matricule_premier_auteur = 5 
+UNION
+SELECT matricule_premier_auteur, titre, date_publication, 'articles de conférence' AS type FROM articles, articles_conferences 
+WHERE articles.url = articles_conferences.url AND articles.matricule_premier_auteur = 5 
+
+-- Récupérer les seconds auteurs si il y en a 
+
+SELECT  nom, prenom, auteurs.matricule FROM seconds_auteurs, auteurs 
+WHERE EXISTS 
+			( SELECT * FROM articles 
+			WHERE articles.url = seconds_auteurs.url and articles.url= 'http://fake.url.com/0000003191.pdf') 
+AND seconds_auteurs.matricule = auteurs.matricule
