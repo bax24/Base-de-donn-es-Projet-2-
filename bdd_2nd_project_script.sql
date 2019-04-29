3796562c67f1b63d8242a3de418da89e5356eec1
MartinL97 <49979727+MartinL97@users.noreply.github.com>
2019-04-29 15:57:56 +0200

    Add files via upload

--- bdd_2nd_project_script.sql
+++ bdd_2nd_project_script.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS bdd_2nd_project;
 CREATE DATABASE bdd_2nd_project CHARACTER SET 'utf8';
 
 Use bdd_2nd_project;
@@ -7,86 +8,94 @@ Use bdd_2nd_project;
 -- ----- CREATION DES TABLES ---------
 -- -----------------------------------
 
-CREATE TABLE Auteur (
-	matricule SMALLINT UNSIGNED NOT NULL,
-	nom VARCHAR(30),
-	prenom VARCHAR(30),
-	debut_doctorat YEAR NOT NULL,
-	nom_institution VARCHAR(40),
-	PRIMARY KEY (matricule)
-);
-
-
-CREATE TABLE Institution (
-	nom VARCHAR(40) NOT NULL,
-	rue VARCHAR(40),
-	numero SMALLINT UNSIGNED,
-	ville VARCHAR(20),
-	pays VARCHAR(30),
-	PRIMARY KEY (nom)
-);
-
-CREATE TABLE Articles (
-	url CHAR(34) NOT NULL,
-	doi BIGINT NOT NULL,
-	titre TEXT,
-	date_publication DATE, -- Format jj/mm/aaaa et non aaaa/mm/jj
-	matricule_1er_auteur SMALLINT UNSIGNED NOT NULL,
-	PRIMARY KEY (url)
-);
-
-CREATE TABLE Sujets (
-	url CHAR(34) NOT NULL,
-	sujet VARCHAR(30),
-	PRIMARY KEY (url,sujet)
-);
-
-CREATE TABLE 2nd_auteur (
-	url CHAR(34) NOT NULL,
-	matricule_2nd_auteur SMALLINT UNSIGNED NOT NULL,
-	PRIMARY KEY (url,matricule_2nd_auteur)
-);
-
-CREATE TABLE Article_journal (
-	url CHAR(34) NOT NULL,
-	pg_debut SMALLINT UNSIGNED,
-	pg_fin SMALLINT UNSIGNED,
-	nom_revue VARCHAR(40) NOT NULL,
-	n_journal SMALLINT UNSIGNED NOT NULL,
-	PRIMARY KEY (url)
-);
-
-CREATE TABLE Article_conference (
-	url CHAR(34) NOT NULL,
-	presentation VARCHAR(25),
-	nom_conference VARCHAR(60),
-	annee_conference YEAR,
-	PRIMARY KEY (url)
-);
-
-CREATE TABLE Revue (
+CREATE TABLE IF NOT EXISTS revues (
 	nom VARCHAR(40) NOT NULL,
 	impact SMALLINT UNSIGNED,
-	PRIMARY KEY (nom)
-);
+	PRIMARY KEY(nom)
+)ENGINE=InnoDB;
 
-CREATE TABLE Conference (
+CREATE TABLE IF NOT EXISTS conferences (
 	nom VARCHAR(60) NOT NULL,
 	annee YEAR NOT NULL,
 	rue VARCHAR(25),
 	numero SMALLINT UNSIGNED,
 	ville VARCHAR(20),
 	pays VARCHAR(20),
-	PRIMARY KEY (nom,annee)
-);
+	PRIMARY KEY(nom,annee)
+)ENGINE=InnoDB;
 
-CREATE TABLE Participations (
+CREATE TABLE IF NOT EXISTS institutions (
+	nom VARCHAR(40) NOT NULL,
+	rue VARCHAR(40),
+	numero SMALLINT UNSIGNED,
+	ville VARCHAR(20),
+	pays VARCHAR(30),
+	PRIMARY KEY(nom)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS auteurs (
+	matricule SMALLINT UNSIGNED NOT NULL,
+	nom VARCHAR(30),
+	prenom VARCHAR(30),
+	debut_doctorat YEAR NOT NULL,
+	nom_institution VARCHAR(40) NOT NULL,
+	PRIMARY KEY(matricule),
+	FOREIGN KEY(nom_institution) REFERENCES institutions(nom)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS articles (
+	url CHAR(34) NOT NULL,
+	doi BIGINT NOT NULL,
+	titre TEXT,
+	date_publication DATE, -- Format jj/mm/aaaa et non aaaa/mm/jj
+	matricule_premier_auteur SMALLINT UNSIGNED NOT NULL,
+	PRIMARY KEY(url),
+	FOREIGN KEY(matricule_premier_auteur) REFERENCES auteurs(matricule)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS sujets_articles (
+	url CHAR(34) NOT NULL,
+	sujet VARCHAR(30),
+	PRIMARY KEY(url,sujet),
+	FOREIGN KEY(url) REFERENCES articles(url)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS seconds_auteurs (
+	url CHAR(34) NOT NULL,
+	matricule SMALLINT UNSIGNED NOT NULL,
+	PRIMARY KEY(url,matricule),
+	FOREIGN KEY(url) REFERENCES articles(url),
+	FOREIGN KEY(matricule) REFERENCES auteurs(matricule)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS articles_journaux (
+	url CHAR(34) NOT NULL,
+	pg_debut SMALLINT UNSIGNED,
+	pg_fin SMALLINT UNSIGNED,
+	nom_revue VARCHAR(40) NOT NULL,
+	n_journal SMALLINT UNSIGNED NOT NULL,
+	PRIMARY KEY(url),
+	FOREIGN KEY(nom_revue) REFERENCES revues(nom)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS articles_conferences (
+	url CHAR(34) NOT NULL,
+	presentation VARCHAR(25),
+	nom_conference VARCHAR(60),
+	annee_conference YEAR,
+	PRIMARY KEY(url),
+	FOREIGN KEY(nom_conference,annee_conference) REFERENCES conferences(nom,annee)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS participations_conferences (
 	matricule SMALLINT UNSIGNED NOT NULL,
 	nom_conference VARCHAR(60),
 	annee_conference YEAR,
 	tarif VARCHAR(12),
-	PRIMARY KEY (matricule,nom_conference,annee_conference)
-);
+	PRIMARY KEY(matricule,nom_conference,annee_conference),
+	FOREIGN KEY(matricule) REFERENCES auteurs(matricule),
+	FOREIGN KEY(nom_conference,annee_conference) REFERENCES conferences(nom,annee)
+)ENGINE=InnoDB;
 
 
 -- ---------------------------------
@@ -94,12 +103,70 @@ CREATE TABLE Participations (
 -- ---------------------------------
 SET GLOBAL local_infile = 1;
 
-LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/auteurs.csv'
-INTO TABLE Auteur
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/revues.csv'
+INTO TABLE revues
 FIELDS TERMINATED BY ';'
-LINES TERMINATED BY '\r\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
-IGNORE 1 LINES
-(matricule,nom,prenom,debut_doctorat,nom_institution);
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
 
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/conferences.csv'
+INTO TABLE conferences
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
 
--- LOAD DATA LOCAL INFILE '/path/pet.txt' INTO TABLE pet;
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/institutions.csv'
+INTO TABLE institutions
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/auteurs.csv'
+INTO TABLE auteurs
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/articles.csv'
+INTO TABLE articles
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/sujets_articles.csv'
+INTO TABLE sujets_articles
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/seconds_auteurs.csv'
+INTO TABLE seconds_auteurs
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/articles_journaux.csv'
+INTO TABLE articles_journaux
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/articles_conferences.csv'
+INTO TABLE articles_conferences
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'C:/Users/Martin/Downloads/Test_SQL/participations_conferences.csv'
+INTO TABLE participations_conferences
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n' -- ou '\r\n' selon l'ordinateur et le programme utilisés pour créer le fichier
+IGNORE 1 LINES;
+
+/*-- LOAD DATA LOCAL INFILE '/path/pet.txt' INTO TABLE pet;
+
+LOAD DATA LOCAL INFILE '/home/ngreffe/WWW/tables/emprunt.csv'
+INTO TABLE emprunt
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;*/
