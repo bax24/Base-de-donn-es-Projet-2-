4a6f3f3a649b9286bf9455f74888f028215331ab
Ludo Bissot <ludinhos@gmail.com>
2019-05-02 16:28:32 +0200

    variables de sessions gérées

--- traitement.php
+++ traitement.php
@@ -1,62 +1,60 @@
+<?php session_start(); ?>
+<?php 
+	if(isset($_POST['login']) AND isset($_POST['passwd']) AND 
+		$_POST['login'] == "group22" AND $_POST['passwd'] == "p"){
+		$_SESSION['user'] = $_POST['login'];
+	}
+	?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" href="style.css" />
-	<title>Scientica</title>
-</head>
-<body>
-
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<title>Scientica</title>
+	</head>
 	<?php 
-	if (isset($_POST['passwd']) AND $_POST['passwd'] ==  "p" AND isset($_POST['login']) AND $_POST['login'] ==  "groupe22") { 
+	if(isset($_SESSION['user'])){
 	?>
-
-	<div class="login">
-		<form method="post">
-			<select name="request"> 
-				<option>Afficher des tables</option>
-				<option>question 2</option>
-				<option>question 3</option>
-			</select>
-			<p>
-				<input type="submit" value="SEARCH" name="search" class="input2">
-			</p>
-		</form>
-
-
-
-	<form method="post" action="request/AfficheTables.php">
-
-			<select name="tables">
-				<option>Auteurs</option>
-				<option>Institutions</option>
-				<option>Articles</option>
-				<option>Sujets</option>
-				<option>Second auteur</option>
-				<option>Articles journal</option>
-				<option>Article conférence</option>
-				<option>Revue</option>
-				<option>Conférence</option>
-				<option>Participation conférence</option>
-			</select>
-		</div>
-	</form>
+		<body>
+			<div class="login">
+				<form method="post">
+					<select name="request"> 
+						<option>Afficher des tables</option>
+						<option>question 2</option>
+						<option>question 3</option>
+					</select>
+					<p>
+						<input type="submit" value="SEARCH" name="search" class="input2">
+					</p>
+				</form>
 
 
+
+			<form method="post" action="traitement.php">
+
+					<select name="tables">
+						<option>Auteurs</option>
+						<option>Institutions</option>
+						<option>Articles</option>
+						<option>Sujets</option>
+						<option>Second auteur</option>
+						<option>Articles journal</option>
+						<option>Article conférence</option>
+						<option>Revue</option>
+						<option>Conférence</option>
+						<option>Participation conférence</option>
+					</select>
+				</div>
+			</form>
+			<div id="logout">
+				<a href='logout.php'> Logout</a>
+			</div>
+		</body>
 	<?php 
-
 	}
-
 	else{
+		echo "Not connected";
+		header("Location: index.php");
+	}
 	?>
-		<p> hey baby u're not in</p>
-
-	<?php  
-		}
-	?>
-
-
-
-
-</body>
 </html>
