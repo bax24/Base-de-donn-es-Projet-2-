25f041a4da59ccda164f02efdc43bbbed5a85e75
Ludo Bissot <ludinhos@gmail.com>
2019-05-04 14:04:12 +0200

    dernière version

--- requests/ArticleMat.php
+++ requests/ArticleMat.php
@@ -3,36 +3,56 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet"  href="../style.css"/>
 	<title>Scientica</title>
 </head>
 <body>
-	<h1>Retrouver mon cul en fct du matricule</h1>
+	<h1>Retrouver l'ensemble des publications d'un chercheur en fonction de son matricule</h1>
 	<?php
-	/*if(isset($_SESSION['user'])){
-		?>
-		
-		<?php*/
 		if(isset($_POST['matricule'])){
 			$bdd = new PDO('mysql:host=ms800.montefiore.ulg.ac.be;dbname=group22;charset=utf8','group22','PKuqfs3Ucm');
-			$req = $bdd->prepare('SELECT publications_chercheurs.titre, publications_chercheurs.date_publication, publications_chercheurs.type, 
-									GROUP_CONCAT(DISTINCT seconds_auteurs.matricule) AS matricules_seconds_auteurs
+			$req = $bdd->prepare('SELECT publications_chercheurs.titre, publications_chercheurs.date_publication, publications_chercheurs.type, seconds_auteurs.matricule
 								FROM seconds_auteurs
-								INNER JOIN(
-									SELECT articles.matricule_premier_auteur, articles.titre, articles.date_publication, "articles de journal" AS type, articles.url
+								RIGHT JOIN(
+									SELECT articles.matricule_premier_auteur, articles.titre, articles.date_publication, "journal" AS type, articles.url
 									FROM articles, articles_journaux 
 									WHERE articles.url = articles_journaux.url 
 										AND articles.matricule_premier_auteur = ? 
 									UNION
-									SELECT articles.matricule_premier_auteur, articles.titre, articles.date_publication, "articles de conférence" AS type, articles.url
+									SELECT articles.matricule_premier_auteur, articles.titre, articles.date_publication, "conférence" AS type, articles.url
 									FROM articles, articles_conferences 
 									WHERE articles.url = articles_conferences.url 
 										AND articles.matricule_premier_auteur = ?
 								) AS publications_chercheurs
-								ON publications_chercheurs.url = seconds_auteurs.url');
+								ON publications_chercheurs.url = seconds_auteurs.url
+								ORDER BY publications_chercheurs.date_publication DESC');
 
 			$req->execute(array($_POST['matricule'], $_POST['matricule']));
-			while ($donnees = $req->fetch())
-				echo '<p> titre' . $donnees['titre'] .  'seconds_auteurs' .$donnees['matricules_seconds_auteurs']. '</p>';
+	?>
+			
+			<div class="Table">
+				<table class="TableResult">
+				        <tr>
+				            <td> <h2>Titre</h2></td>
+							<td><h2>Date de publication</h2></td>
+							<td><h2>Type</h2></td>
+							<td><h2>Matricule(s) second(s) auteur(s)</h2></td>
+				        </tr>
+				        <?php
+				    		while ($donnees = $req->fetch()){
+									echo("<tr>");
+									echo("<td>".$donnees['titre']."</td>");
+									echo("<td>".$donnees['date_publication']."</td>");
+									echo("<td>".$donnees['type']."</td>");
+									echo("<td>".$donnees['matricule']."</td>");
+									echo("</tr>");
+							}
+						?>
+						
+				</table>
+			</div>
+		<?php
 		}
 		else{
 			?>
@@ -45,12 +65,6 @@
 			</div>
 			<?php
 		}
-
-	/*}
-	else{
-		echo "Not connected";
-		header("Location: index.php")
-	}*/
 
  	?>
 </body>
